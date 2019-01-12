745a9af7dcc7ba383dd166915aed633496947cbb
wright13 <s.wright.618@gmail.com>
2019-01-11 17:39:58 -0800

    Add function to replace a placeholder in a snippet with an actual value

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// Takes an HTML snippet with named "properties" surrounded by double curly
+// braces and replaces the specified property with a value
+function insertIntoSnippet(snippet, property, value) {
+  var propertyToReplace = "{{" + property + "}}";
+  var newSnippet = snippet;
+  newSnippet = newSnippet.replace(new RegExp(propertyToReplace, "g"), value);
+  return newSnippet;
+};
